d151b43ca2c47f4cf83b70f81e78514d1db73070
DecrepitSloth <Knowlesaa77@gmail.com>
2023-03-27 12:41:38 -0500

    move div creation to exterior function that also adds event listeners to change grid colors

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Create a 16x16 div grid
 
-for (i=0;i<256;i++) {
+function create() {
 let box = document.createElement("div");
 document.getElementById("container").appendChild(box); 
 box.classList.add("grid");
+box.addEventListener("mouseover", func)
+    function func() {
+        box.setAttribute("style", "background-color: black;")
+    }
+}
+
+for (i=0;i<256;i++) {
+    create(); 
 }
