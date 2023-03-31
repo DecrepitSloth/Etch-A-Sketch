2ffba0ee6a581091de7b4a170b511736d9cf1770
DecrepitSloth <Knowlesaa77@gmail.com>
2023-03-31 13:01:27 -0500

    add prompt to gridSelect() to pick how many squares per side and then run create(). Add remove() that runs in gridSelect() to reset the grid to default before applying create()

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ box.addEventListener("mouseover", func)
 
 // Create a 16x16 div grid
 
-for (i=0;i<256;i++) {
-    create(); 
+document.getElementById("size").addEventListener("click", gridSelect);
+
+function gridSelect() {
+    remove();
+    let y = prompt("Select how many grids per side you want.");
+    total = y * y;
+    for (i=0; i<total; i++) {
+        create(); 
+    }   
+}
+
+// Remove the previous grid
+
+function remove() {
+    let element = document.getElementById("container");
+    while (element.firstChild) {
+    element.removeChild(element.firstChild);
+    }
 }
