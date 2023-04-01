02e90cdca3fefd7b010e9195fb65e46922c1e7d1
DecrepitSloth <Knowlesaa77@gmail.com>
2023-04-01 12:55:07 -0500

    move lines 8-15 down to 23-27 as well as renamed variable boot to percentage to reflect its purpose

--- script.js
+++ script.js
@@ -4,31 +4,27 @@ function create() {
 let box = document.createElement("div");
 document.getElementById("container").appendChild(box); 
 box.classList.add("grid");
-
-perc = Math.round((960/boot) - 2);
-document.getElementsByClassName("grid");
-box.style.flexBasis = perc + "px";
-
-box.addEventListener("mouseover", func)
-    function func() {
-        box.setAttribute("style", "background-color: black;")
-    }
 }
 
 // Create a 16x16 div grid
 
 document.getElementById("size").addEventListener("click", gridSelect);
 
-let boot = 0;
+let input = 0;
 
 function gridSelect() {
     remove();
-    let y = prompt("Select how many grids per side you want.", boot);
+    let y = prompt("Select how many grids per side you want.", input);
     total = y * y;
-    boot = y;
-    for (i=0; i<total; i++) {
+    input = y;
+        for (i=0; i<total; i++) {
         create(); 
-    }   
+        }
+    perc = (960/input) - 2;
+    let percentage = document.getElementById("container").children;
+        for (i = 0; i < percentage.length; i++) {
+        percentage[i].style.width = perc + "px";
+        }
 }
 
 // Remove the previous grid
